e7baa56cd7a93be755516aee21069da594ab4cdf
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-28 11:42:10 +0200

    Create check_bot_1.py

---
+++ check_bot_1.py
@@ -0,0 +1,54 @@
+import requests
+
+def get_api_key(file_path='api_key.txt'):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+# Get the API key from the text file
+api_key = get_api_key()
+
+if api_key:
+    # Define the endpoint URL
+    url = 'https://vast.ai/api/v0/instances/'
+
+    # Define the headers, including the API key for authentication
+    headers = {
+        'Authorization': 'ApiKey ' + api_key
+    }
+
+    # Define the query parameters
+    params = {
+        'user': 'self'  # To get instances belonging to the authenticated user
+    }
+
+    # Make the GET request
+    response = requests.get(url, headers=headers, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        instances = response.json()
+        
+        # Print information about each instance
+        print("Your Instances:")
+        for instance in instances:
+            print("Instance ID:", instance['id'])
+            print("GPU Name:", instance['machine']['gpu_name'])
+            print("Dollars Per Hour (DPH):", instance['dph_total'])
+            print("SSH Information:")
+            print("  Host:", instance['ssh_host'])
+            print("  Port:", instance['ssh_port'])
+            print("  Username:", instance['ssh_username'])
+            print("  Password:", instance['ssh_password'])
+            print("-" * 30)
+    else:
+        print("Failed to retrieve instances. Status code:", response.status_code)
+else:
+    print("API key could not be retrieved.")
