5b68584437164cce35fd8e4b81e590e43622f328
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-28 22:14:55 +0200

    Update check_bot_1.py

--- check_bot_1.py
+++ check_bot_1.py
@@ -183,7 +183,8 @@ for ssh_info in ssh_info_list:
     else:
         logging.error("Failed to retrieve log information for instance ID: %s", instance_id)
 
-
+# Sort the data by "Blocks/$" in increasing order
+table_data.sort(key=lambda x: x[6] if x[6] is not None else float('-inf'))
 
 # Print the table
 print_table(table_data)
