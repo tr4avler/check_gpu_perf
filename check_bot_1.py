b2f95bfa702788e947449a4cd31b1d273bf40a69
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-29 08:17:58 +0100

    Update check_bot_1.py

--- check_bot_1.py
+++ check_bot_1.py
@@ -84,6 +84,8 @@ def clean_ansi_codes(input_string):
     ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]', re.IGNORECASE)
     return ansi_escape.sub('', input_string)
 
+import re
+
 def get_log_info(ssh_host, ssh_port, username):
     private_key_path = "/home/admin/.ssh/id_ed25519"
     
@@ -107,16 +109,21 @@ def get_log_info(ssh_host, ssh_port, username):
         last_line = clean_ansi_codes(last_line)
         
         # Parse the last line to get the required information
-        pattern = re.compile(r'Mining:.*\[(\d+):(\d+):(\d+),.*(?:Details=normal:(\d+))?')
+        pattern = re.compile(r'Mining:.*\[(\d+):(\d+):(\d+),.*(?:Details=normal:(\d+)|Details=xuni:(\d+)).*\]')
         match = pattern.search(last_line)
         if match:
             # Extracting the running time and normal blocks
-            hours, minutes, seconds, normal_blocks = match.groups()
-            normal_blocks = int(normal_blocks) if normal_blocks is not None else 0
-            return int(hours), int(minutes), int(seconds), normal_blocks
+            hours, minutes, seconds, normal_blocks, xuni_blocks = match.groups()
+            blocks = int(normal_blocks) if normal_blocks is not None else int(xuni_blocks) if xuni_blocks is not None else None
+            
+            if blocks is not None:
+                return int(hours), int(minutes), int(seconds), blocks
+            else:
+                logging.error("Failed to extract block information")
+                return None, None, None, None
         else:
-            logging.warning("Failed to parse the log line or missing 'Details=normal:' information")
-            return 0, 0, 0, 0
+            logging.error("Failed to parse the log line")
+            return None, None, None, None
         
     except Exception as e:
         logging.error("Failed to connect or retrieve log info: %s", e)
@@ -125,20 +132,21 @@ def get_log_info(ssh_host, ssh_port, username):
     finally:
         ssh.close()
 
+
 from prettytable import PrettyTable      
-def print_table(data, output_file='manual_log1.txt'):
+def print_table(data, output_file='table_output.txt'):
     # Define the table and its columns
     table = PrettyTable()
-    table.field_names = ["Instance ID", "GPU Name", "DPH", "Runtime (hours)", "Block/h", "Blocks/$"]
+    table.field_names = ["Instance ID", "GPU Name", "DPH", "XNM Blocks", "Runtime (hours)", "Block/h", "$/Blocks"]
     
-    # Add rows to the table
+    # Add rows to the table to console
     for row in data:
         table.add_row(row)
 
-    # Print the table to console
+    # Print the table
     print(table)
 
-   # Write the table to a text file
+    # Write the table to a text file
     with open(output_file, 'w') as f:
         f.write(str(table))
     print(f"Table also written to {output_file}")
@@ -157,23 +165,31 @@ table_data = []
 # Fetch Log Information for Each Instance
 for ssh_info in ssh_info_list:
     instance_id = ssh_info['instance_id']
-    gpu_name = ssh_info['gpu_name']  # Corrected this line
-    dph_total = ssh_info['dph_total']  # Corrected this line
+    gpu_name = ssh_info['gpu_name']
+    dph_total = float(ssh_info['dph_total'])  # Convert DPH to float for calculations
     ssh_host = ssh_info['ssh_host']
     ssh_port = ssh_info['ssh_port']
 
     logging.info("Fetching log info for instance ID: %s", instance_id)
     hours, minutes, seconds, normal_blocks = get_log_info(ssh_host, ssh_port, username)
     
-    total_hours, total_minutes, total_seconds, normal_blocks = get_log_info(ssh_host, ssh_port, username)
-
-    if total_hours is not None:
-        runtime_hours = total_hours + total_minutes / 60 + total_seconds / 3600
-        logging.info("Running Time: %d hours, %d minutes, %d seconds", total_hours, total_minutes, total_seconds)
+    if hours is not None:
+        runtime_hours = hours + minutes / 60 + seconds / 3600
+        logging.info("Running Time: %d hours, %d minutes, %d seconds", hours, minutes, seconds)
         logging.info("Normal Blocks: %d", normal_blocks)
-        table_data.append([instance_id, gpu_name, dph_total, round(runtime_hours, 2), "", ""])
+
+        # Calculate Block/h and handle the case when runtime is zero
+        block_per_hour = normal_blocks / runtime_hours if runtime_hours != 0 else 0
+
+        # Calculate Blocks/$ and handle the case when the number of blocks is zero
+        blocks_per_dollar = (runtime_hours * dph_total) / normal_blocks if normal_blocks != 0 else 0
+        
+        table_data.append([instance_id, gpu_name, round(dph_total, 4), normal_blocks, round(runtime_hours, 2), round(block_per_hour, 2), round(blocks_per_dollar, 2)])
     else:
         logging.error("Failed to retrieve log information for instance ID: %s", instance_id)
+
+# Sort the data by "Blocks/$" in increasing order
+table_data.sort(key=lambda x: x[6] if x[6] is not None else float('-inf'))
 
 # Print the table
 print_table(table_data)
