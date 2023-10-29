cba373e030da4a77d0e802ca21dd3acda28ee0f4
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-29 16:32:19 +0100

    Update check_bot_1.py

--- check_bot_1.py
+++ check_bot_1.py
@@ -109,7 +109,7 @@ def get_log_info(ssh_host, ssh_port, username):
         last_line = clean_ansi_codes(last_line)
         
         # Parse the last line to get the required information
-        pattern = re.compile(r'Mining:.*\[(\d+):(\d+):(\d+),.*(?:normal:(\d+)|xuni:(\d+)).*Difficulty=(\d+).*\]')
+        pattern = re.compile(r'Mining:.*\[(\d+):(\d+):(\d+),.*(?normal:(\d+)|xuni:(\d+)).*Difficulty=(\d+).*\]')
         match = pattern.search(last_line)
         if match:
             # Extracting the running time and normal blocks
