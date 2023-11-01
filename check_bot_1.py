431cd1236e4fb349f232d06169d098e5366a5926
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-11-01 15:45:14 +0100

    Update check_bot_1.py

--- check_bot_1.py
+++ check_bot_1.py
@@ -171,12 +171,13 @@ def get_log_info(ssh_host, ssh_port, username):
         last_line = clean_ansi_codes(last_line)
         
         # Parse the last line to get the required information
-        pattern = re.compile(r'Mining:.*\[(\d+):(\d+):(\d+),.*(?:\? Blocks/s|.*(?:Details=super:(\d+)|Details=normal:(\d+)|Details=xuni:(\d+))).*HashRate:(\d+.\d+).*Difficulty=(\d+).*\]')
+        pattern = re.compile(r'Mining:.*\[(?:(\d+):)?(\d+):(\d+),.*(?:Blocks/s|.*(?:Details=(?:super:(\d+)|normal:(\d+)|xuni:(\d+)))).*HashRate:(\d+\.\d+).*Difficulty=(\d+).*\]')
         match = pattern.search(last_line)
         if match:
             # Extracting the running time and blocks information
             hours, minutes, seconds, super_blocks, normal_blocks, xuni_blocks, hash_rate, difficulty = match.groups()
-
+            
+            hours = int(hours) if hours is not None else 0
             super_blocks = int(super_blocks) if super_blocks is not None else 0
             normal_blocks = int(normal_blocks) if normal_blocks is not None else 0
             xuni_blocks = int(xuni_blocks) if xuni_blocks is not None else 0
